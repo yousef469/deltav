ae0557c337e1961a929a92250cce0c9b89dd13d7
yousef469 <youseflovemessi41@gmail.com>
2026-02-13 00:37:50 +0300

    chore: Apply Nova v8.0 definitively

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinks = [
     { name: "Services", href: "#services" },
     { name: "Philosophy", href: "#philosophy" },
     { name: "Work", href: "#work" },
-    { name: "Nova (APK)", href: "https://github.com/yousef469/deltav/releases/download/v6.0/app-debug.apk" },
+    { name: "Nova (APK)", href: "https://github.com/yousef469/deltav/releases/download/v8.0/app-debug.apk" },
 ]
 
 export function Navbar() {
